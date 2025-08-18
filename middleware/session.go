006411e679011959e4b56d012e8ef0f4e575dbec
Henrique Goncalves <kamus@hadenes.io>
2025-08-17 21:32:51 -0300

    fix: secure cookies

--- middleware/session.go
+++ middleware/session.go
@@ -13,11 +13,12 @@ func init() {
 	gob.Register(&models.User{})
 	gob.Register(&models.Flash{})
 	Store.Options.HttpOnly = true
+	Store.Options.Secure = true
 	// This sets the maxAge to 5 days for all cookies
 	Store.MaxAge(86400 * 5)
 }
 
 // Store contains the session information for the request
 var Store = sessions.NewCookieStore(
-	[]byte(securecookie.GenerateRandomKey(64)), //Signing key
+	[]byte(securecookie.GenerateRandomKey(64)), // Signing key
 	[]byte(securecookie.GenerateRandomKey(32)))
